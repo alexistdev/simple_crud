939001b87088bc774297761adadd3c44452a605b
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-12-07 20:44:33 +0700

    feat(fe-movielist): create movie list component

--- FE/movie-project/src/app/models/genre.model.ts
+++ FE/movie-project/src/app/models/genre.model.ts
@@ -1,2 +1,4 @@
 export class Genre {
+  id?:number;
+  name?:string;
 }
--- FE/movie-project/src/app/models/movie.model.ts
+++ FE/movie-project/src/app/models/movie.model.ts
@@ -1,6 +1,10 @@
+import {Genre} from "./genre.model";
+import {Type} from "@angular/core";
+
 export class Movie {
   id?: number;
   title?: string;
   director?: string;
   summary?: string;
+  genres?: Genre[];
 }
