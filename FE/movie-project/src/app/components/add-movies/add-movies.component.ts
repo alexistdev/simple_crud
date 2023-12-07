b79843dcb108d7384d7409345dcbb678e21f4cd5
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-12-07 23:35:23 +0700

    feat(fe-add): fetch data genre to multiple select

--- FE/movie-project/src/app/components/add-movies/add-movies.component.ts
+++ FE/movie-project/src/app/components/add-movies/add-movies.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import {Genre} from "../../models/genre.model";
+import {MovieService} from "../../services/movie.service";
 
 @Component({
   selector: 'app-add-movies',
   templateUrl: './add-movies.component.html',
   styleUrl: './add-movies.component.css'
 })
-export class AddMoviesComponent {
+export class AddMoviesComponent implements OnInit {
+  genreList?: Genre[];
 
+  constructor(private movieService: MovieService) {
+  }
+  ngOnInit(): void {
+    this.getDataGenre();
+  }
+
+  getDataGenre(): void {
+    this.movieService.getGenre()
+      .subscribe({
+        next: (result) => {
+          this.genreList = result;
+          console.log(result);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
--- FE/movie-project/src/app/services/movie.service.ts
+++ FE/movie-project/src/app/services/movie.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from "../models/movie.model";
+import {Genre} from "../models/genre.model";
 
-const baseUrl = 'http://localhost:8901/api/lists';
+const movie_url = 'http://localhost:8901/api/lists';
+const genre_url = 'http://localhost:8901/api/genre';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,10 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(baseUrl);
+    return this.http.get<Movie[]>(movie_url);
+  }
+
+  getGenre(): Observable<Genre[]>{
+    return this.http.get<Genre[]>(genre_url);
   }
 }
